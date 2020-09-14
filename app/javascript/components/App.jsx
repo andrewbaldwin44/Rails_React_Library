fe4356654582428b63dc0d26e2bc712288bf1385
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-09-14 19:16:45 -0400

    Add CSS reset

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Routes from '../routes/Index';
+import styled from 'styled-components';
+
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
     <>
-      {Routes}
+      <GlobalStyles />
+      <Body>
+        {Routes}
+      </Body>
     </>
   )
 }
+
+const Body = styled.div`
+  position: absolute;
+  top: 0;
+`;
 
 export default App;
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Home() {
   return (
     <>
-      Cool story bro
+      Home
     </>
   )
 }
