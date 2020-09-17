2e3444fb366efdd1efd3f2d9fb8aa185deb03a37
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-09-17 12:18:51 -0400

    Add firebase authentication context

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import AuthenticationProvider from '../components/AuthenticationContext';
 import App from '../components/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <AuthenticationProvider>
+      <App />
+    </AuthenticationProvider>,
     document.getElementById('root'),
   )
 })
