5ad1af9beba45e760a9d293af04a99fa34773488
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-09-19 17:35:06 -0400

    Add landing page

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProtectedRoute from './ProtectedRoute';
 
+import Home from '../components/Home';
 import Login from '../components/Login/Index';
 import Library from '../components/Library';
 import Profile from '../components/Profile';
@@ -10,13 +11,14 @@ import FourOhFour from '../components/FourOhFour';
 export default (
   <Router>
     <Switch>
-      <Route path="/" exact>
+      <Route path='/' exact component={Home} />
+      <Route path='/login' exact>
           <Login accountCreated={true} />
       </Route>
-      <Route path="/sign_up" exact component={Login} />
-      <ProtectedRoute path="/library" exact component={Library} />
-      <Route path="/users/profile" exact component={Profile} />
-      <Route path="/*" exact component={FourOhFour} />
+      <Route path='/sign_up' exact component={Login} />
+      <ProtectedRoute path='/library' exact component={Library} />
+      <Route path='/users/profile' exact component={Profile} />
+      <Route path='/*' exact component={FourOhFour} />
     </Switch>
   </Router>
 )
