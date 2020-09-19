f82a375611a5e6bca90f96b2054bbe9296c1c471
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-09-19 16:03:12 -0400

    Add 404 page

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '../components/Login/Index';
 import Library from '../components/Library';
 import Profile from '../components/Profile';
+import FourOhFour from '../components/FourOhFour';
 
 export default (
   <Router>
@@ -14,6 +15,7 @@ export default (
       <Route path="/sign_up" exact component={Login} />
       <Route path="/library" exact component={Library} />
       <Route path="/users/profile" exact component={Profile} />
+      <Route path="/*" exact component={FourOhFour} />
     </Switch>
   </Router>
 )
