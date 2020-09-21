586b888872c0510a47e268010614a64fd4c8d11d
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-09-20 20:50:37 -0400

    Add book adding form

--- app/javascript/components/BookMenu.jsx
+++ app/javascript/components/BookMenu.jsx
@@ -1,20 +1,140 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { BsBook } from 'react-icons/bs';
+import { FaBookOpen } from 'react-icons/fa';
+
 function BookMenu() {
+  const submitBook = event => {
+    event.preventDefault();
+  }
+
   return (
-    <Wrapper>
+    <Wrapper className='menu-overlay'>
+
+      <Form onSubmit={submitBook}>
+        <fieldset>
+          <label htmlFor='title'>Title</label>
+          <input
+            name='title'
+            id='title'
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor='author'>Author</label>
+          <input
+            name='author'
+            id='author'
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor='genre'>Genre</label>
+          <input
+            name='genre'
+            id='genre'
+          />
+        </fieldset>
+
+        <fieldset className='centered-field'>
+          <ReadBook
+            control={<Checkbox icon={<BsBook />} checkedIcon={<FaBookOpen className='checked' />} name='read-book' />}
+            label='Read'
+          />
+        </fieldset>
+
+        <fieldset className='centered-field'>
+          <Shelve type='submit'>Shelve</Shelve>
+        </fieldset>
+      </Form>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
   position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100vw;
   height: 100vh;
   top: 0;
   left: 0;
   background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const Form = styled.form`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 70%;
+
+  fieldset {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    label {
+        padding: 10px 5px;
+        font-size: 1.1em;
+    }
+
+    input {
+      color: black;
+      padding: 10px;
+      border-radius: 3px;
+      width: 390px;
+
+      &:focus {
+        border: 2px solid var(--light-green);
+      }
+    }
+  }
+
+  .centered-field {
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
+`;
+
+const ReadBook = styled(FormControlLabel)`
+  display: flex;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+  width: 100px;
+
+  svg {
+    color: white;
+    font-size: 1.6em;
+    margin: 0 15px;
+  }
+
+  span {
+    font-size: 1.2em;
+  }
+
+  .checked {
+    color: var(--light-green);
+  }
+`;
+
+const Shelve = styled.button`
+  height: 50px;
+  width: 170px;
+  border: 2px solid white;
+  border-radius: 3px;
+  transition: all 0.4s ease-in-out;
+
+  &:hover {
+    border-color: var(--light-green);
+    box-shadow: 0 0 5px 1px #69f0ae;
+  }
 `;
 
 export default BookMenu;
