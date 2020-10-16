621937b38ca73c4959241ddb760c7026cf738963
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-10-15 23:00:25 -0400

    Add basic style to homepage

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -4,11 +4,35 @@ import styled from 'styled-components';
 
 function Home() {
   return (
-    <div>
-      Welcome
+    <Wrapper>
+      <h1>Welcome!</h1>
       <Link to='login'>Login</Link>
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  background-color: rgba(0, 0, 0, 0.5);
+  height: 400px;
+  width: 400px;
+  line-height: 2;
+  font-size: 1.6em;
+  font-weight: bold;
+
+  h1 {
+    text-decoration: underline;
+    font-size: 1.6em;
+    margin-bottom: 20px;
+  }
+`;
 
 export default Home;
